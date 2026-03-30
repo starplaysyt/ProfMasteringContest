ca469bdf00002b4c280d0573f679331b195eee7a
starplaysyt <horaudmitry@gmail.com>
2026-03-30 13:16:38 +0300

    And the final part of that checkpoint - two pages and Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
+using ProfMasteringProject.Persistence;
+using ProfMasteringProject.Repositories;
+using ProfMasteringProject.Services;
+using ProfMasteringProject.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<FileStorageSettings>(
+    builder.Configuration.GetSection("FileStorage"));
+
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 10 * 1024 * 1024; // 10 MB
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<PictureEntityRepository>();
+builder.Services.AddScoped<UserEntityRepository>();
+
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<IFileService, FileService>();
 
 var app = builder.Build();
 
@@ -13,6 +37,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
@@ -20,10 +50,9 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+app.UseStatusCodePagesWithReExecute("/error?error={0}");
+app.MapControllers().WithStaticAssets();
+
 
 
 app.Run();
